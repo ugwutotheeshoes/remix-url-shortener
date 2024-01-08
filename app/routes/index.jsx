8500365805492851558495667921d979ec5ab507
walterobrian <148688458+walterobrian@users.noreply.github.com>
2024-01-08 02:09:53 +0100

    Update index.jsx

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,6 +1,6 @@
 import "../style/index.css";
 import React, { useState } from "react";
-import { Client, Databases, ID } from "appwrite";
+import { Client, Functions } from "appwrite";
 
 export default function Index() {
   const [show, setShow] = useState(false);
@@ -12,25 +12,29 @@ export default function Index() {
     setLongUrl(e.target.value);
   };
 
-  const client = new Client();
-  const databases = new Databases(client);
-  client
-    .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
-    .setProject("656e2c43a574f664ebd8"); // Your project ID
+  const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject("PROJECT_ID");
+  const functions = new Functions(client);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const promise = databases.createDocument(
-      "656e2d6d5ee9c0a6c953",
-      "656e2f4485d720af38f2",
-      ID.unique(),
+
+    const execution = functions.createExecution(
+      "FUNCTION_ID",
       JSON.stringify({
         url: longUrl,
-      })
+      }),
+      false,
+      "/",
+      "POST",
+      {
+        "Content-Type": "application/json",
+      }
     );
-    promise.then(
+    execution.then(
       function (response) {
-        setShortUrl(response.$id);
+        setShortUrl(response.responseBody);
         setShow(true);
         setLongUrl("");
         console.log(response);
@@ -57,8 +61,10 @@ export default function Index() {
       </form>
       {show && (
         <p>
-          <span><b>Here's your short link: </b></span>
-          http://6577636fce6343804854.appwrite.global/{shortUrl}
+          <span>
+            <b>Here's your short link: </b>
+          </span>
+          {shortUrl}
         </p>
       )}
     </div>
